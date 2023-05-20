79c3f1a0d150d3b5e9c78e537787a796296dca6c
Szymon Hyziak <szymon.hyziak@protonmail.com>
2023-05-20 21:38:56 +0200

    Organizing files

---
+++ scripts/epsg_codes.py
@@ -0,0 +1,11 @@
+epsg_codes = {
+    "pseudo_mercator": 3857,
+    "world_geodetic_system": 4326,
+    "poland_cs2000": {
+        "15_meridian": 2176,
+        "18_meridian": 2177,
+        "21_meridian": 2178,
+        "24_meridian": 2179,
+    },
+    "poland_cs1992": 2180
+}
