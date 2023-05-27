5f0b65b0267163ef924b5be3d207f0d6f45a1bd9
Szymon Hyziak <szymon.hyziak@protonmail.com>
2023-05-27 20:06:33 +0200

    Changing font and text color globally

--- scripts/layouts.py
+++ scripts/layouts.py
@@ -1,53 +1,126 @@
 import PySimpleGUI as sg
+from styles import font, text_color, dark_color
 
 input_crs = [
-    [sg.Text("Welcome to the CRS converter!"), ],
-    [sg.Text("Select the input CRS:"), ],
-    [sg.Text("[1] Pseudo-Mercator"), ],
-    [sg.Text("[2] World Geodetic System"), ],
-    [sg.Text("[3] Poland CS2000"), ],
+    [sg.Text("Welcome to the CRS converter!", 
+        font=font, 
+        text_color=text_color, 
+        background_color=dark_color
+    ), ],
+    [sg.Text("Select the input CRS:", 
+        font=font, 
+        text_color=text_color
+    ), ],
+    [sg.Text("[1] Pseudo-Mercator", 
+        font=font, 
+        text_color=text_color
+    ), ],
+    [sg.Text("[2] World Geodetic System", 
+        font=font, 
+        text_color=text_color
+    ), ],
+    [sg.Text("[3] Poland CS2000", 
+        font=font, 
+        text_color=text_color
+    ), ],
     [
-        sg.Text("Enter your choice:"),
-        sg.InputText(),
+        sg.Text("Enter your choice:", 
+            font=font, 
+            text_color=text_color
+        ),
+        sg.InputText(
+            font=font, 
+            text_color=dark_color
+        ),
     ],
     [
-        sg.Button("Confirm"),
-        sg.Button("Cancel"),
+        sg.Button("Confirm", 
+            font=font
+        ),
+        sg.Button("Cancel", 
+            font=font
+        ),
     ],
 ]
 
 output_crs = [
-    [sg.Text("Welcome to the CRS converter!"), ],
-    [sg.Text("Select the output CRS:"), ],
-    [sg.Text("[1] Pseudo-Mercator"), ],
-    [sg.Text("[2] World Geodetic System"), ],
-    [sg.Text("[3] Poland CS2000"), ],
+    [sg.Text("Welcome to the CRS converter!",
+        font=font, 
+        text_color=text_color
+    ), ],
+    [sg.Text("Select the output CRS:", 
+        font=font, 
+        text_color=text_color
+    ), ],
+    [sg.Text("[1] Pseudo-Mercator", 
+        font=font, 
+        text_color=text_color
+    ), ],
+    [sg.Text("[2] World Geodetic System", 
+        font=font, 
+        text_color=text_color
+    ), ],
+    [sg.Text("[3] Poland CS2000", 
+        font=font, 
+        text_color=text_color
+    ), ],
     [
-        sg.Text("Enter your choice:"),
-        sg.InputText(),
+        sg.Text("Enter your choice:", 
+            font=font, 
+            text_color=text_color
+        ),
+        sg.InputText(
+            font=font, 
+            text_color=dark_color
+        ),
     ],
     [
-        sg.Button("Confirm"),
-        sg.Button("Cancel"),
+        sg.Button("Confirm", 
+            font=font
+        ),
+        sg.Button("Cancel", 
+            font=font
+        ),
     ],
 ]
 
-result = [
+coords = [
     [
-        sg.Text("Enter the latitude: "),
-        sg.InputText(),
+        sg.Text("Enter the latitude: ", 
+            font=font, 
+            text_color=text_color
+        ),
+        sg.InputText(
+            font=font, 
+            text_color=dark_color
+        ),
     ],
     [
-        sg.Text("Enter the longitude: "),
-        sg.InputText(),
+        sg.Text("Enter the longitude: ", 
+            font=font, 
+            text_color=text_color
+        ),
+        sg.InputText(
+            font=font, 
+            text_color=dark_color
+        ),
     ],
     [
-        sg.Button("Confirm"),
-        sg.Button("Cancel"),
+        sg.Button("Confirm", 
+            font=font
+        ),
+        sg.Button("Cancel", 
+            font=font
+        ),
     ],
 ]
 
 error = [
-    [sg.Text("Script received invalid input ❌"), ],
-    [sg.Button("Cancel"), ],
+    [sg.Text("Script received invalid input ❌",
+        font=font, 
+        text_color=text_color
+    ), ],
+    [sg.Button("Cancel", 
+        font=font
+    ), ],
 ]
---
+++ scripts/styles.py
@@ -0,0 +1,3 @@
+font = ("Fixedsys", 12)
+text_color = "#efefef"
+dark_color = "#2a2a2a"
